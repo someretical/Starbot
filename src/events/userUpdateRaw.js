09010bf3c5f095702621ef7e5f915ec0c903ade6
thecraptheorist <thecraptheorist@gmail.com>
2020-08-22 17:18:23 +1000

    replaced null with undefined and tidied up spacing

--- src/events/userUpdateRaw.js
+++ src/events/userUpdateRaw.js
@@ -2,12 +2,12 @@
 
 module.exports = async (client, user) => {
 	const userObj = client.users.cache.get(user.id);
-	if (!userObj) return null;
+	if (!userObj) return undefined;
 
 	await userObj.add();
 
 	const { username, discriminator } = userObj.data;
-	if (user.username === username && user.discriminator === discriminator) return null;
+	if (user.username === username && user.discriminator === discriminator) return undefined;
 
 	return userObj.queue(() => client.db.models.User.upsert({
 		id: user.id,
