5fc6c8806f3de5257f24c016ee842781b8b913f5
Yankai Zhu <29365738+someretical@users.noreply.github.com>
2020-09-04 19:04:56 +1000

    added hooks and implmented more efficient caching (#22)

--- src/models/Guild.js
+++ src/models/Guild.js
@@ -3,10 +3,15 @@
 const { DataTypes, Model } = require('sequelize');
 const { db } = require('../structures/StarbotDatabase.js');
 const queue = new (require('../structures/StarbotQueueManager.js'))();
+const cache = new (require('discord.js').Collection)();
 
 class Guild extends Model {
 	static get q() {
 		return queue;
+	}
+
+	static get cache() {
+		return cache;
 	}
 }
 
@@ -48,6 +53,30 @@ Guild.init({
 		type: DataTypes.JSON,
 		defaultValue: [],
 	},
-}, { db });
+}, {
+	hooks: {
+		// Emitted on model class methods with find keyword
+		afterFind: val => {
+			if (!val) return;
+
+			if (Array.isArray(val) && val.length) {
+				val.map(instance => !Guild.cache.has(instance.id) ? Guild.cache.set(instance.id, instance) : undefined);
+			} else if (!Guild.cache.has(val.id)) {
+				Guild.cache.set(val.id, val);
+			}
+		},
+
+		// Emitted on model instances that are destroyed
+		// Model classes require custom handling for destructive actions such as destroy() and bulkDelete()
+		afterDestroy: instance => Guild.cache.delete(instance.id),
+
+		// Emitted on model instances that have save() or update() called on them
+		afterSave: instance => Guild.cache.set(instance.id, instance),
+
+		// Emitted on model class method upsert()
+		afterUpsert: ([instance]) => Guild.cache.set(instance.id, instance),
+	},
+	sequelize: db,
+});
 
 module.exports = Guild;
--- src/models/OptOut.js
+++ src/models/OptOut.js
@@ -3,11 +3,16 @@
 const { DataTypes, Model } = require('sequelize');
 const { db } = require('../structures/StarbotDatabase.js');
 const queue = new (require('../structures/StarbotQueueManager.js'))();
+const cache = new (require('discord.js').Collection)();
 const User = require('./User.js');
 
 class OptOut extends Model {
 	static get q() {
 		return queue;
+	}
+
+	static get cache() {
+		return cache;
 	}
 }
 
@@ -25,7 +30,33 @@ OptOut.init({
 		type: DataTypes.STRING,
 		defaultValue: 'None',
 	},
-}, { db });
+}, {
+	hooks: {
+		// Emitted on model class methods with find keyword
+		afterFind: val => {
+			if (!val) return;
+
+			if (Array.isArray(val) && val.length) {
+				val.map(instance =>
+					!OptOut.cache.has(instance.user_id) ? OptOut.cache.set(instance.user_id, instance) : undefined,
+				);
+			} else if (!OptOut.cache.has(val.user_id)) {
+				OptOut.cache.set(val.user_id, val);
+			}
+		},
+
+		// Emitted on model instances that are destroyed
+		// Model classes require custom handling for destructive actions such as destroy() and bulkDelete()
+		afterDestroy: instance => OptOut.cache.delete(instance.user_id),
+
+		// Emitted on model instances that have save() or update() called on them
+		afterSave: instance => OptOut.cache.set(instance.user_id, instance),
+
+		// Emitted on model class method upsert()
+		afterUpsert: ([instance]) => OptOut.cache.set(instance.user_id, instance),
+	},
+	sequelize: db,
+});
 
 User.hasMany(OptOut, { foreignKey: 'user_id' });
 User.hasMany(OptOut, { foreignKey: 'executor_id' });
