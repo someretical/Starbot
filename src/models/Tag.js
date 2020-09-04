9c017823271263f062f8b18d7fe27b2e2324742a
someretical <29365738+someretical@users.noreply.github.com>
2020-09-04 19:04:56 +1000

    added hooks and more efficient caching (#22)

--- src/models/Tag.js
+++ src/models/Tag.js
@@ -3,6 +3,7 @@
 const { DataTypes, Model } = require('sequelize');
 const { db } = require('../structures/StarbotDatabase.js');
 const queue = new (require('../structures/StarbotQueueManager.js'))();
+const cache = new (require('discord.js').Collection)();
 const Guild = require('./Guild.js');
 const User = require('./User.js');
 
@@ -10,16 +11,20 @@ class Tag extends Model {
 	static get q() {
 		return queue;
 	}
+
+	static get cache() {
+		return cache;
+	}
 }
 
 Tag.init({
 	id: {
-		type: DataTypes.INTEGER,
-		autoIncrement: true,
+		type: DataTypes.UUIDV4,
 		primaryKey: true,
+		defaultValue: DataTypes.UUIDV4,
 		allowNull: false,
 	},
-	guild_id: {
+	Tag_id: {
 		type: DataTypes.STRING,
 		allowNull: false,
 	},
@@ -43,11 +48,40 @@ Tag.init({
 		type: DataTypes.INTEGER,
 		defaultValue: 0,
 	},
-}, { db });
+}, {
+	hooks: {
+		// Emitted on model class methods with find keyword
+		afterFind: val => {
+			if (!val) return;
 
-Guild.hasMany(Tag, { foreignKey: 'guild_id' });
+			if (Array.isArray(val) && val.length) {
+				val.map(instance => !Tag.cache.has(instance.id) ? Tag.cache.set(instance.id, instance) : undefined);
+			} else if (!Tag.cache.has(val.id)) {
+				Tag.cache.set(val.id, val);
+			}
+		},
+
+		// Emitted on model instances that are destroyed
+		// Model classes require custom handling for destructive actions such as destroy() and bulkDelete()
+		afterDestroy: instance => Tag.cache.delete(instance.id),
+
+		// Emitted on model instances that have save() or update() called on them
+		afterSave: instance => Tag.cache.set(instance.id, instance),
+
+		// Bandage fix for https://github.com/sequelize/sequelize/issues/10823
+		beforeUpsert: values => {
+			values.id = DataTypes.UUIDV4;
+		},
+
+		// Emitted on model class method upsert()
+		afterUpsert: ([instance]) => Tag.cache.set(instance.id, instance),
+	},
+	sequelize: db,
+});
+
+Guild.hasMany(Tag, { foreignKey: 'Tag_id' });
 User.hasMany(Tag, { foreignKey: 'creator_id' });
 Tag.belongsTo(User, { foreignKey: 'creator_id' });
-Tag.belongsTo(Guild, { foreignKey: 'guild_id' });
+Tag.belongsTo(Guild, { foreignKey: 'Tag_id' });
 
 module.exports = Tag;
