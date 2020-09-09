2c45953651a6a337d29da08228dd1f9193f920ae
Yankai Zhu <29365738+someretical@users.noreply.github.com>
2020-09-10 09:21:02 +1000

    remove node-fetch and chalk

--- src/util/Logger.js
+++ src/util/Logger.js
@@ -1,28 +1,27 @@
 'use strict';
 
-const chalk = require('chalk');
 const moment = require('moment');
 const { inspect } = require('util');
 
 class Logger {
 	static log(...args) {
-		args.map(arg => Logger.write(inspect(arg).toString(), chalk.bgWhite.black('[DEBUG]')));
+		args.map(arg => Logger.write(inspect(arg).toString(), '\u001b[40m\u001b[37m[DEBUG]\u001b[0m'));
 	}
 
 	static info(message) {
-		Logger.write(message, chalk.bgGreen('[INFO]'));
+		Logger.write(message, '\u001b[42m[INFO]\u001b[0m');
 	}
 
 	static warn(message) {
-		Logger.write(message, chalk.bgYellow('[WARN]'));
+		Logger.write(message, '\u001b[43m[WARN]\u001b[0m');
 	}
 
 	static err(message) {
-		Logger.write(message, chalk.bgRed('[ERROR]'), true);
+		Logger.write(message, '\u001b[41m[ERROR]\u001b[0m', true);
 	}
 
 	static stack(err) {
-		Logger.write(err, chalk.bgRed('[STACK]'), true);
+		Logger.write(err.stack, '\u001b[41m[STACK]\u001b[0m', true);
 	}
 
 	static write(content, type, error = false) {
