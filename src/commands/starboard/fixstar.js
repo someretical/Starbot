491c1fa6f818bc8323b40b731c6624538ff76a28
Yankai Zhu <29365738+someretical@users.noreply.github.com>
2020-09-25 22:28:17 +1000

    optimised url checking

--- src/commands/starboard/fixstar.js
+++ src/commands/starboard/fixstar.js
@@ -2,7 +2,6 @@
 
 const StarbotCommand = require('../../structures/StarbotCommand.js');
 const { matchMessageURL } = require('../../util/Util.js');
-const { invalid } = require('moment');
 
 module.exports = class FixStar extends StarbotCommand {
 	constructor(client) {
@@ -16,32 +15,37 @@ module.exports = class FixStar extends StarbotCommand {
 				optional: false,
 				description: 'link to a message',
 				example: 'https://discord.com/channels/361736003859513344/732842050516680705/732842074612826112',
-				code: true,
 			}],
 			aliases: ['updatestar'],
 			userPermissions: [],
 			clientPermissions: [],
 			guildOnly: true,
 			ownerOnly: false,
-			throttle: 10000,
+			throttle: 20000,
 		});
 	}
 
 	async run(message) {
 		const { args, channel, guild } = message;
-		const invalidURL = () => channel.embed('Please provide a valid message URL!');
-		let url;
+		const invalidURL = () => channel.send('Please provide a valid message URL!');
 
-		if (!args[0]) return invalidURL();
+		if (!args[0]) {
+			return channel.send('Please provide a message URL!');
+		}
 
-		url = matchMessageURL(url);
-		if (!url) return invalid();
+		const obj = matchMessageURL(args[0], true);
+		if (!obj) return invalidURL();
 
-		const starChannel = guild.channels.cache.get(url.channel_id);
+		const starChannel = guild.channels.cache.get(obj.channel_id);
 		if (!starChannel || starChannel.type !== 'text') return invalidURL();
 
-		const starMessage = await starChannel.messages.fetch(url.message_id);
-		if (!starMessage) return invalidURL();
+		let starMessage;
+		try {
+			starMessage = await starChannel.messages.fetch(obj.message_id);
+		} catch (err) {
+			return invalidURL();
+		}
+		if (!starMessage) return undefined;
 
 		await guild.starboard.fixStar(starMessage);
 
