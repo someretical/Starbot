ff4cf47737db6aa644ed09913ed06eb33c0b2f22
Yankai Zhu <29365738+someretical@users.noreply.github.com>
2020-09-25 22:09:52 +1000

    renamed command and implemented #22

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,6 +5,14 @@ const Logger = require('../util/Logger.js');
 module.exports = async client => {
 	Logger.info(`Logged in as ${client.user.tag} (${client.user.id})`);
 
+	await client.user.setPresence({
+		status: 'online',
+		afk: false,
+		activity: {
+			name: `@${client.user.username} help`,
+			type: 'PLAYING',
+		},
+	});
 	await client.users.fetch(client.user.id);
 	await client.db.models.User.q.add(client.user.id, () =>
 		client.db.models.User.findCreateFind({ where: { id: client.user.id } }),
