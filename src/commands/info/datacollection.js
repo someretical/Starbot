29c05c9b0eb042beea5146a7ac52e4bb8ddd1cea
someretical <29365738+someretical@users.noreply.github.com>
2020-09-26 11:31:31 +1000

    update data collection policy

--- src/commands/info/datacollection.js
+++ src/commands/info/datacollection.js
@@ -3,6 +3,60 @@
 const { stripIndents, oneLine } = require('common-tags');
 const StarbotCommand = require('../../structures/StarbotCommand.js');
 
+const embed = (client, prefix) => client.embed(null, true)
+	.setTitle('What user data the bot collects')
+	.setDescription(stripIndents`
+		${oneLine`
+			• User IDs - these [snowflakes](https://github.com/twitter-archive/snowflake)
+			uniquely identify users and can be used to obtain user information 
+			if the bot is in any server that the user is also in.
+			They are also publicly accessible to anyone who can access your profile.
+		`}
+		• Tag names + responses - the bot stores the names and responses of your tags on applicable servers.
+		${oneLine`
+			• Starred messages - the bot stores the message ID and
+			reactors of your starred messages on applicable servers.
+		`}
+		• Blocking - the bot only stores the IDs of blocked users
+		• Throttling - the bot stores user IDs to enable the throttling of commands with long cooldowns.
+
+		${oneLine`
+			If you wish to delete all your data stored by the bot, you can run the
+			\`${prefix}purgedata\` command.
+		`}
+
+		${oneLine`
+			If you wish to completely opt out of participating with the bot,
+			you can run the \`${prefix}opt-out\` command. This command is **NOT REVERSIBLE**.
+		`}
+	`)
+	.addField('Extra notes', stripIndents`
+		• If you opt out, your user ID will still be stored in the reactors of starred messages by other users.
+		${oneLine`
+			• The \`${prefix}purgedata\` command will delete everything about you with the
+			exception of the above and will reset your user coins and reputation to 0.
+		`}
+		${oneLine`
+			• The \`${prefix}opt-out\` command will delete everything about you with the 
+			exception of the above and will reset your user coins and reputation to 0.
+		`}
+		${oneLine`
+			• It will also create an entry with your ID in it so the bot knows to block you in the future.
+		`}
+		${oneLine`
+			• If a user is not in the same server as you, they will not be able to see guild specific data
+			about you such as stars and tags.
+		`}
+		${oneLine`
+			• Every measure has been taken to prevent the erosion of privacy
+			from occurring to your data once you opt out.
+		`}
+		${oneLine`
+			• All the data described above is what the bot will save to secondary storage. 
+			Other data may be used but not saved while the bot is running.
+		`}
+	`);
+
 module.exports = class DataCollection extends StarbotCommand {
 	constructor(client) {
 		super(client, {
@@ -20,61 +74,14 @@ module.exports = class DataCollection extends StarbotCommand {
 		});
 	}
 
-	run(message) {
-		const { client, channel, guild } = message;
+	async run(message) {
+		const { client, author, channel, guild } = message;
 
-		const embed = client.embed(null, true)
-			.setTitle('What user data the bot collects')
-			.setDescription(stripIndents`
-				${oneLine`
-					• User IDs - these [snowflakes](https://github.com/twitter-archive/snowflake)
-					uniquely identify users and can be used to obtain user information 
-					if the bot is in any server that the user is also in.
-					They are also publicly accessible to anyone who can access your profile.
-				`}
-				• Usernames + disriminators - what makes up your discord username.
-				• Tag names + responses - the bot stores the names and responses of your tags on applicable servers.
-				${oneLine`
-					• Starred messages - the bot stores the message content and
-					reactors of your starred messages on applicable servers.
-				`}
-				• Blocking - the bot stores the blocked user's ID, the command executor's ID as well as the reason.
-				• Throttling - the bot stores user IDs to enable the throttling of commands with long cooldowns.
-
-				${oneLine`
-					If you wish to delete all your data stored by the bot, you can run the
-					\`${guild.settings.prefix}purgedata\` command.
-				`}
-
-				${oneLine`
-					If you wish to completely opt out of participating with the bot,
-					you can run the \`${guild.settings.prefix}opt-out\` command. This command is **NOT REVERSIBLE**.
-				`}
-			`)
-			.addField('Extra notes', stripIndents`
-				• Your user ID will still be stored in the reactors for starred messages by other users.
-				${oneLine`
-					• The \`${guild.settings.prefix}purgedata\` command will delete everything about you with the
-					exception of the above and will reset your user coins and reputation to 0.
-				`}
-				${oneLine`
-					• The \`${guild.settings.prefix}opt-out\` command will delete everything about you with the 
-					exception of the above and will reset your user coins and reputation to 0.
-				`}
-				${oneLine`
-					• It will also create an entry with your ID in it so the bot knows to not collect anymore data
-					about you in the future.
-				`}
-				${oneLine`
-					• If a user is not in the same server as you, they will not be able to see guild specific data
-					about you such as stars and tags.
-				`}
-				${oneLine`
-					• Every measure has been taken to prevent the erosion of privacy
-					from occurring to your data once you opt out.
-				`}
-			`);
-
-		channel.send(embed);
+		try {
+			await author.send(embed(client, guild.model.prefix));
+			channel.send('Please check your DM\'s with the bot.');
+		} catch (err) {
+			channel.send('Please enable direct messages to view the data collection policy.');
+		}
 	}
 };
